2dafd0ff2164e22eb6f848c1064cc4df630e2c58
Marko <ilya1501@gmail.com>
2022-10-18 18:01:09 +0300

    Prostokvashino

---
+++ HW-13/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_13
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+
+        public Person(string name, string profession, int age, string street, int house, string recipient)
+        {
+            Name = name;
+            Profession = profession;
+            Age = age;
+            Address = new Address()
+            {
+                Street = street,
+                House = house,
+                Recipient = recipient,
+            };
+        }
+
+        
+    }
+}
