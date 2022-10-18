4194d7489958729441f928d8822b9ea9bba010fa
Marko <ilya1501@gmail.com>
2022-10-18 18:01:38 +0300

    Menu

---
+++ HW-13/GG.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_13
+{
+   public class GG
+    { /*
+        public void Menu()
+        {
+            int choose = 1;
+            string Write;
+            string Read;
+
+            Console.WriteLine("Сhoose a function: Write | Read");
+            string text = Console.ReadLine();
+
+          
+        } */
+
+        public void changeperson()
+        {
+
+        }
+    }
+}
